c41df849372bae8b63f7a25cfd9a2859dc7e6932
murilo varoto <murilovaroto@gmail.com>
2025-06-06 11:06:56 -0300

    configurando rota da api: port 3000

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,11 @@
-const 
+const express = require('express');
+const app     = express();
+const PORT    = process.env.PORT || 3000;
+
+app.get('/', ( req, res ) => {
+    res.send('Bem vindo a api de tarefas')
+})
+
+app.listen( PORT, () => {
+    console.log( `Servidor rodando na porta ${PORT}` )
+})
