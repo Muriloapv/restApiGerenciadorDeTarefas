cafa3598a029624737b3726e1d7e3299c7d713ac
murilo varoto <murilovaroto@gmail.com>
2025-06-06 14:18:04 -0300

    Configuração da unit routes, arquivo responsavel por realizar as operações da controller, fazendo a ligação entre os metodos do crud e as funções criada

---
+++ backend/routes/tarefaRoutes.js
@@ -0,0 +1,11 @@
+const express = require( 'express' );
+const router = express.Router();
+const tarefaController = require( '../controllers/tarefaController' );
+
+//Rotas para operações CRUD 
+router.get( '/tarefas', tarefaController.listarTarefas );
+router.post( '/tarefas', tarefaController.criarTarefa );
+router.put( '/tarefas/:id', tarefaController.atualizarTarefa );
+router.delete( '/tarefas/:id', tarefaController.excluirTarefa );
+
+module.exports = router;
