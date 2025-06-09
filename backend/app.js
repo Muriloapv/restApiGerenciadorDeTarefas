a2c422a8085df73d684e01dea4e5ef28044a86fc
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2025-06-08 22:05:58 -0300

    create userRouter: unit responsavel por ligar as funçõas da controllers com o padrões https do crud ( get, post, put, delete )

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const app     = express();
+const express      = require('express');
+const app          = express();
 const tarefaRoutes = require( './routes/tarefaRoutes' );
-const PORT    = process.env.PORT || 3000;
+const userRoutes   = require( './routes/userRoutes'   );
+const PORT         = process.env.PORT || 3000;
 
 app.use( express.json() );
-app.use('/api', tarefaRoutes );
+app.use( '/api', tarefaRoutes );
+app.use( '/api', userRoutes   );
 
 app.listen( PORT, () => {
     console.log( `Servidor on-line na porta: ${PORT}` )
     console.log(`URL: http://localhost:${PORT}/api/tarefas`);
-})
+});
 
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = requrie ( 'express' );
+const router = express.Router();
+const userController = require( '../controllers/userController' );
+
+//Rotas para operações CRUD
+router.get    ( '/user'    , userController.listarUsuarios   );
+router.post   ( '/user'    , userController.criarUsuario     );
+router.put    ( '/user/:id', userController.atualizarUsuario );
+router.delete ( '/user/:id', userController.excluirUsuario   );
+
+module.exports = router;
+
