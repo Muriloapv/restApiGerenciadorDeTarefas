32415a29d9936ab6d451df5921e5bcd8d9e510a0
murilo varoto <murilovaroto@gmail.com>
2025-06-09 11:40:52 -0300

    configuração swagger

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,29 @@ const tarefaRoutes = require( './routes/tarefaRoutes' );
 const userRoutes   = require( './routes/userRoutes'   );
 const PORT         = process.env.PORT || 3000;
 
+const swaggerUi    = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Tarefas',
+      version: '1.0.0',
+      description: 'API documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // files containing annotations as above
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use( express.json() );
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use( '/api', tarefaRoutes );
 app.use( '/api', userRoutes   );
 
@@ -16,6 +38,9 @@ app.listen( PORT, () => {
     console.log( '|------------------------------------------------| '  );
     console.log( '|--------- Gerenciador de usuarios---------------| '  );
     console.log( `| URL: http://localhost:${PORT}/api/user            |`);
+    console.log( '|------------------------------------------------| '  );
+    console.log( '|----------------- Link Swagger -----------------| '  );
+    console.log( `| URL: http://localhost:${PORT}/api-docs            |`);
     console.log( '|------------------------------------------------| '  ) ;
     
 });
